a57512005408ea12f77e1a5eff09776d899e66d5
idalmas <ian.dalmas@icloud.com>
2024-12-10 16:22:01 -0800

    fix type saftey on "Database"

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,7 +5,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 // Create a Supabase client with the service role key
-export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseServiceRoleKey, {
   auth: {
     autoRefreshToken: false,
     persistSession: false
