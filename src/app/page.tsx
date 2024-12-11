ab23977d048041f18c4c51200ab4ca4d046ddf74
idalmas <ian.dalmas@icloud.com>
2024-12-10 20:56:00 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className={`min-h-screen bg-white ${inter.className}`}>
+    <div className={`min-h-screen bg-white selection:bg-black selection:text-white ${inter.className}`}>
       {/* Hero Section */}
       <div className="relative isolate">
         {/* Full-width image container */}
